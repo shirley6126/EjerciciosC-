45c0088d5d021544c6215e176cc228958f01d6cb
shirley6126 <shirley.daya1406@gmail.com>
2026-03-14 22:00:12 -0500

    Ejercicio 8

---
+++ Ejercicio8.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+	int main(){
+    int lista[3] = {18, 12, 25};
+
+    // Recorrer la lista con un for
+    for(int i = 0; i < 3; i++){
+        cout << lista[i] << " ";
+    }
+
+    return 0;
+}
+
